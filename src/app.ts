bac4fca7659ae1b818372f3787fa0f4023ba7975
Nihal Ahammed <nahammed@smart-infrastructure.com>
2025-04-25 13:02:34 +0530

    feat: add routing for authentication endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import indexReoutes from './routes/index.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', indexReoutes);
 
 export default app;
